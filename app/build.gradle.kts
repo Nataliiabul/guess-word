73468b666684a55ae506404561013fcebc166f26
Nataliia <Natasha.bulo4nikova@yandex.ru>
2026-02-18 22:20:39 +0300

    add colors + start code xml game activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // scale
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
 }
