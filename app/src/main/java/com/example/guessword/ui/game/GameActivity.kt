acc8bc34938d179f93f8a867bd4cba1f73c6522b
Nataliia <Natasha.bulo4nikova@yandex.ru>
2026-02-25 21:33:33 +0300

    Add ViewModel logic to GameActivity

--- app/src/main/java/com/example/guessword/ui/game/GameActivity.kt
+++ app/src/main/java/com/example/guessword/ui/game/GameActivity.kt
@@ -5,17 +5,56 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.guessword.R
+import com.example.guessword.data.repository.WordRepository
+import com.example.guessword.databinding.ActivityGameBinding
+import kotlinx.coroutines.launch
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGameBinding
+    private lateinit var viewModel: GameViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(
+            this, object: ViewModelProvider.Factory{
+                override fun <T : ViewModel> create(modelClass : Class<T>): T{
+                    return GameViewModel(WordRepository()) as T
+                }
+            }
+        ).get(GameViewModel::class.java)
+
+        lifecycleScope.launch{
+            viewModel.uiState.collect {
+                    state -> updateUI(state)
+            }
+        }
+
+        binding.btnNewWord.setOnClickListener{
+            viewModel.loadNewWord()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    private fun updateUI(state: GameUIState){
+        println("Слово: ${state.currentWord.text}")
+        println("Перемешанные буквы: ${state.shuffledLetters}")
+        println("Слоты пользователя: ${state.userSlots}")
+        println("Победа: ${state.isWordGuessed}")
+        println("---")
+    }
+
 }
