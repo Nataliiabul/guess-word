9b7b8d10f6de21623bb2c15c4ed93a29bb7ddd0e
Nataliia <Natasha.bulo4nikova@yandex.ru>
2026-02-25 20:53:52 +0300

    start GameViewModel logic done

--- app/src/main/java/com/example/guessword/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/guessword/ui/game/GameViewModel.kt
@@ -1,7 +1,33 @@
 package com.example.guessword.ui.game
 
 import androidx.lifecycle.ViewModel
+import com.example.guessword.data.model.Word
 import com.example.guessword.data.repository.WordRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel(private val repository: WordRepository): ViewModel() {
+    private val _uiState = MutableStateFlow(GameUIState(
+        Word("", emptyList<Char>()),
+        emptyList<Char>(),
+        emptyList<Char?>(),
+        false
+    ))
+
+    val uiState: StateFlow<GameUIState> = _uiState.asStateFlow()
+
+    init {
+        loadNewWord()
+    }
+
+    fun loadNewWord() {
+        val word = repository.getRandomWord()
+        _uiState.value = GameUIState(
+            word,
+            word.letters.shuffled(),
+            List(word.letters.size){null},
+            false
+        )
+    }
 }
